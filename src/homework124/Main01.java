4015a473576ee0aba9c77d548f9e68854276e3f3
Dennis Schönberg <dennis_schoenberg@yahoo.de>
2023-11-06 19:14:23 +0100

    init

---
+++ src/homework124/Main01.java
@@ -0,0 +1,9 @@
+package homework124;
+
+public class Main01 {
+    public static void main(String[] args) {
+        String text = "Hello world!";
+        char[] arrStr = text.toCharArray();
+        System.out.println(arrStr[0]);
+    }
+}
