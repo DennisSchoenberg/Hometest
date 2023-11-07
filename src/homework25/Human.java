bb2c0beb333f1d36eefe88f6f50c45f23f7ef11d
Dennis Schönberg <dennis_schoenberg@yahoo.de>
2023-11-07 21:24:14 +0100

    Homework25 upgrade

---
+++ src/homework25/Human.java
@@ -0,0 +1,12 @@
+package homework25;
+
+public class Human {
+    String name;
+    int age;
+
+    public void introduce() {
+        System.out.println("Hallo, my name is " + name + ". I am " + age + " years old.");
+    }
+
+}
+
--- src/homework25/Main01.java
+++ src/homework25/Main01.java
@@ -1,5 +1,16 @@
 package homework25;
 
 public class Main01 {
+    public static void main(String[] args) {
+        Human Johaness = new Human();
+        Johaness.name = "Johaness";
+        Johaness.age = 43;
 
+        Human Eva = new Human();
+        Eva.name = "Eva";
+        Eva.age = 25;
+
+        Johaness.introduce();
+        Eva.introduce();
+    }
 }
